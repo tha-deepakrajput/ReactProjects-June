95b8495570bb5493dc35f888a24270505e65dcd8
tha-deepakrajput <thadeepakrajput@Deepaks-MacBook-Air.local>
2025-06-22 14:30:17 +0530

    password generator created now you can create the password and copy as well.

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += '0123456789';
+    if (charAllowed) str += '!@#$%^&*()_-=+{}|/<>~`';
+
+    for (let i = 1; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+
+
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 6);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-10 py-3 text-center text-yellow-500 bg-gray-700">
+        <h1 className="text-center text-white my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 bg-white text-black"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+
+          <button 
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0">Copy</button>
+
+        </div>
+
+        <div className="flex text-sm gap-x-1">
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="range"
+            value={length}
+            min={6}
+            max={100}
+            className="cursor-pointer" 
+            onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label>Length: {length} </label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox" 
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {setNumberAllowed((prev) => !prev)}}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox" 
+            defaultChecked={charAllowed}
+            id="charInput"
+            onChange={() => {setCharAllowed((prev) => !prev)}}
+            />
+            <label htmlFor="charInput">Char</label> 
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
